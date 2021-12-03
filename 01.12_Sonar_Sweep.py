07ba7f0f117503a095b071263fc27f4470f16d38
Laura <44951156+xxvazquez@users.noreply.github.com>
2021-12-04 00:06:27 +0100

    Update 01.12_Sonar_Sweep.py

--- 01.12_Sonar_Sweep.py
+++ 01.12_Sonar_Sweep.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 
+# PART I
 # create the new dir
 current_dir = os.getcwd()
 new_dir = current_dir + '/inputs/' 
@@ -9,11 +10,11 @@ new_dir = current_dir + '/inputs/'
 col_name = ['Measurement']
 measurements = pd.read_csv(new_dir + '01.12.csv', names=col_name)
 
-# convert the dataframe into a list
-measurements_list = measurements.values.tolist()
-
 # define a function to count larger and smaller values 
-def count_measurements_type(list_name):
+def count_measurements_type(df_name, column):
+    # convert the dataframe into a list
+    list_name = df_name[column].values.tolist()
+    # set the counter to 0 
     i_larger = 0
     i_smaller = 0
     # iterate over the list
@@ -26,8 +27,16 @@ def count_measurements_type(list_name):
             else:
                 i_smaller += 1
     # print the values
-    print('Smaller values:', i_smaller)
-    print('Larger values:', i_larger)
+    print('Smaller:', i_smaller)
+    print('Larger:', i_larger)
 
 # call the function with the list name that was defined before
-count_measurements_type(measurements_list)
+print('Measurements:')
+count_measurements_type(measurements, 'Measurement')
+
+
+# PART II
+# add one column with the sum of three records
+measurements['sum'] = measurements['Measurement'].rolling(3).sum()
+print('Sum:')
+count_measurements_type(measurements, 'sum')
