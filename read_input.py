1c3ec0957d95e7fa2596c3e4fab56fdcd0f64096
Laura <44951156+xxvazquez@users.noreply.github.com>
2021-12-04 01:21:21 +0100

    Add files via upload
    Function that reads a file and converts it to a data frame

---
+++ read_input.py
@@ -0,0 +1,15 @@
+import os
+import pandas as pd
+
+# define a function that reads a file and converts it to a dataframe
+def read_file(file_name, col_names):
+    # create the new dir
+    current_dir = os.getcwd()
+    new_dir = current_dir + '/inputs/' 
+    # define a global dataframe
+    global df
+    # read the values from the file and assign them to a df
+    df = pd.read_csv(new_dir + file_name, names=col_names)
+    return(df)
+    
+
