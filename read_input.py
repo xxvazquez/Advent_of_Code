d7ca1bab718c566027d0be977827c4885deec546
Laura <44951156+xxvazquez@users.noreply.github.com>
2021-12-06 21:58:48 +0100

    Update read_input.py

--- read_input.py
+++ read_input.py
@@ -2,14 +2,12 @@ import os
 import pandas as pd
 
 # define a function that reads a file and converts it to a dataframe
-def read_file(file_name, col_names):
+def read_file(file_name, col_names, converters=None):
     # create the new dir
     current_dir = os.getcwd()
     new_dir = current_dir + '/inputs/' 
     # define a global dataframe
     global df
     # read the values from the file and assign them to a df
-    df = pd.read_csv(new_dir + file_name, names=col_names)
+    df = pd.read_csv(new_dir + file_name, names=col_names, converters=converters)
     return(df)
-    
-
